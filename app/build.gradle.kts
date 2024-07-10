77dc945e407017f45ddbb38e53c7ee3fe1bdfb26
akrilly <yasunovv@ya.ru>
2024-07-11 05:16:38 +1100

    R8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.yasunov.shiftappintern"
         minSdk = 24
+
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -22,11 +23,15 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        resourceConfigurations.addAll(listOf("en", "ru"))
+
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
